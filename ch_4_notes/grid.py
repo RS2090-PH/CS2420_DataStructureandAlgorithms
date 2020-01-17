8df27253d106fc75ce0553e22059ca4f40810c10
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-01-17 16:13:01 -0700

    Chapter Notes

---
+++ ch_4_notes/grid.py
@@ -0,0 +1,34 @@
+"""
+Author: Ken Lambert
+""""
+
+from array import array
+
+class Grid(object):
+    """ Represents a twodimensional array. """
+    def __init__(self, rows, columns, filValue = None):
+        self.data = Array(rows)
+        for  row in range(rows):
+            self.data[row] = Array(columns, fillValue)
+
+    def getHeight(self):
+        """ Returns the number of rows. """
+        return len(self.data)
+
+    def getWidth(self):
+        """ Returns the number of columns. """
+        return len(self.data[0])
+
+    def __getitem__(self, index):
+        """ Supports two-dimensional indexing
+        with [row][column]. """
+        return self.data[index]
+
+    def __str__(self):
+        """ Returns a string representation of the grid. """
+        result = ""
+        for row in range(self.getHeight()):
+            for col in range(self.getWidth()):
+                results += str(self.data[row][col]) + " "
+            results += "\n"
+        return result

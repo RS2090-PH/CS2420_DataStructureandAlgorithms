2896d5e0c8397f8b2fcc5732ffc3f5a951c890f8
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-01-21 11:59:34 -0700

    Project two start

---
+++ proj_2/sort.py
@@ -0,0 +1,22 @@
+from recursioncounter import RecursionCounter
+
+def selection_sort(lyst):
+
+
+def insertion_sort(lyst):
+
+
+def mergesort(lyst):
+    RecursionCounter() 
+
+def quicksort(lyst):
+    RecursionCounter() 
+
+def is_sorted(lyst):
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()

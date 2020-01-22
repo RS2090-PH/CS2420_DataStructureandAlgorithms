323a46404d1e6e2ac1d127a6c60e9125bf0cefc6
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-01-21 23:08:08 -0700

    Update insertion_sort.py

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -29,3 +29,55 @@ def main():
 if __name__ == "__main__":
     main()
         
+        
+        
+        
+        
+        
+        
+def insertion_sort(lyst, left, right):
+    held = lyst[0]
+
+    while i < len(lyst):
+        item_to_insert = lyst[i]
+        j = i - 1
+
+        while j >= 0:
+            if item_to_insert < lyst[j]:
+                lyst[j + 1] = lyst[j]
+                j -= 1
+            else:
+                break
+        lyst[j + 1] = item_to_insert
+        i += 1
+        
+        
+    held[0] = lyst[0]
+    temp = 1
+    while left < right:
+        if held[0] > lyst[temp]:
+            held.append(lyst[temp])
+            
+        
+        
+        
+        
+        left += 1
+        
+        
+        
+def insertion_sort(lyst):
+    i = 1
+
+    while i < len(lyst):
+        item_to_insert = lyst[i]
+        j = i - 1
+
+        while j >= 0:
+            if item_to_insert < lyst[j]:
+                lyst[j + 1] = lyst[j]
+                j -= 1
+            else:
+                break
+        lyst[j + 1] = item_to_insert
+        i += 1

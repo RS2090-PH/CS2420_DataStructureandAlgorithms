e3a269e41de7bc72bfa34b6c71989449c5d70c7a
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-01-30 20:35:40 -0700

    Exercise work

---
+++ ch_7_notes/ex_7_9/ex_7_9.py
@@ -0,0 +1,11 @@
+"""
+Exercise code located in maze.py file.
+
+Write a program that solves the maze problem discussed earlier in this chapter. You should use the Grid class developed in Chapter 4 in this problem.
+
+Take a text file as an input for the maze's description.
+Display the maze based off the description.
+Program attempts to find solution.
+Displays the result.
+Displays the maze again.
+"""

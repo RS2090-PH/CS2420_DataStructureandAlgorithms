23976f8f66c21d9577c0758c8960562b5aa8a2d0
RS2090-PH <57384116+RS2090-PH@users.noreply.github.com>
2020-03-07 01:19:01 -0700

    exercise work

---
+++ ch_10_notes/ex_10_7/ex_10_7.py
@@ -0,0 +1,6 @@
+"""
+Complete the node classes, LeafNode and InteriorNode,
+for the expression tree developed in this chapter. A main()
+has been provided to test the implementation of LeafNode
+and InteriorNode.
+"""
